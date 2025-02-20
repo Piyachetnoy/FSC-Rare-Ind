652d438f7888e7db57f014883bccc3e351cbd937
Piyachetnoy <piyachet2310noy@gmail.com>
2025-02-21 05:41:52 +0900

    fine tuning

--- model.py
+++ model.py
@@ -23,6 +23,25 @@ class Resnet50FPN(nn.Module):
         feat['map4'] = feat_map4
         return feat
 
+class FamNet(nn.Module):
+    def __init__(self):
+        super(FamNet, self).__init__()
+        self.resnet = torchvision.models.resnet50(pretrained=False)  # Do not use pre-trained weights
+        children = list(self.resnet.children())
+        self.conv1 = nn.Sequential(*children[:4])
+        self.conv2 = children[4]
+        self.conv3 = children[5]
+        self.conv4 = children[6]
+
+    def forward(self, im_data):
+        feat = OrderedDict()
+        feat_map = self.conv1(im_data)
+        feat_map = self.conv2(feat_map)
+        feat_map3 = self.conv3(feat_map)
+        feat_map4 = self.conv4(feat_map3)
+        feat['map3'] = feat_map3
+        feat['map4'] = feat_map4
+        return feat
 
 class CountRegressor(nn.Module):
     def __init__(self, input_channels,pool='mean'):
